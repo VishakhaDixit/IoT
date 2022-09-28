67b46a04795b2962348803040faad5bfd960d006
VishakhaDixit <vdixit2@uncc.edu>
2022-09-28 15:45:20 -0400

    Create PkgThread class for defining run() to execute a thread using Dijkstras Algorithm.

---
+++ Assignment3/FedexPkgDeliverySystem/src/PkgThread.java
@@ -0,0 +1,80 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class PkgThread extends Thread
+{
+    boolean run = true;
+	public static final String[] locationArr = {"Northborough, MA","Edison, NJ","Pittsburgh, PA","Allentown, PA","Martinsburg, WV",
+                                                "Charlotte, NC","Atlanta, GA","Orlando, FL","Memphis, TN","Grove City, OH","Indianapolis, IN",
+                                                "Detroit, MI","New Berlin, WI", "Minneapolis, MN", "St. Louis, MO", "Kansas, KS", "Dallas, TX", 
+                                                "Houston, TX","Denver, CO","Salt Lake City, UT","Phoenix, AZ","Los Angeles, CA","Chino, CA",
+                                                "Sacramento, CA","Seattle, WA"};
+	String location, currLocation;
+	Thread t = new Thread();
+	Package p;
+	ArrayList<Integer> path = new ArrayList<Integer>();
+	
+    public PkgThread(Package p) 
+    {
+		this.p = p;
+	}
+
+    public void startThread() throws SQLException 
+    {
+		Connection conn = FedexDB.getConn();
+		FedexDB.addPckgDet(conn, p);	
+		
+		PkgThread t = new PkgThread(p);
+		t.start();
+	}
+
+	public void run() 
+    {
+		while(run) 
+        {	
+			int src = 0, dest = 0;
+            DijkstrasAlgorithm algo = new DijkstrasAlgorithm();
+			
+            for(int i = 0; i < locationArr.length; i++) 
+            {
+				location = locationArr[i];
+				if(location.equals(p.getSrc())) 
+                {
+					src = i; 
+				}
+				if(location.equals(p.getDest())) 
+                {
+					dest = i;
+				}
+			}
+			path = algo.getShortestPath(src, dest);
+			
+			for (int i = 0;i < path.size(); i++) 
+            { 
+				currLocation = locationArr[path.get(i)];
+				p.setCurrLocation(currLocation);
+				
+                if(currLocation.equals(p.getDest())) 
+                {
+					run = false;
+				}
+				
+                //Store it to Fedex DB
+				Connection con = FedexDB.getConn();
+				try 
+                {
+					FedexDB.updateCurrLocation(con, p);
+					System.out.println("Location = " + p.getCurrLocation() + ", " + "TrackingNumber = " + p.trackingNo);
+					TimeUnit.SECONDS.sleep(5);
+				} 
+                catch (InterruptedException | SQLException e) 
+                {
+					System.out.println("Error in Timer");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
