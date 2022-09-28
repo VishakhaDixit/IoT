2649ea22774548bfe53b5fbcbbf558100941bd53
VishakhaDixit <vdixit2@uncc.edu>
2022-09-28 15:49:28 -0400

    Modify main() method to read Packages.txt file and create a list of newly received packages for delivery. Spwan a new thread for processing of each package using PkgThread class.

--- Assignment3/FedexPkgDeliverySystem/src/Main.java
+++ Assignment3/FedexPkgDeliverySystem/src/Main.java
@@ -1,5 +1,62 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.io.File;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        Scanner sc = null;
+        // Open MyPackages file to get list of all received packages for delivery.
+        try 
+        {
+            System.out.println("Opening File\n");
+            File file = new File("Packages.txt");
+            sc = new Scanner(file);
+            sc.useDelimiter(" - ");
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("File not found!!!");
+            return;
+        }
+
+        ArrayList<Package> pkgList = new ArrayList<Package>();
+        while (sc.hasNext()) 
+        {
+			String weight = sc.next();
+			String dimensions = sc.next();
+			int pieces = sc.nextInt();
+			String src = sc.next();
+			String dest= sc.next();
+			String signServices = sc.next();
+			String packaging = sc.next();
+			String specialHandling = sc.next();
+			String service = sc.next();
+            String trackingNum = sc.next();
+			
+            Package p = new Package(weight, dimensions, pieces, src, dest, signServices, packaging, specialHandling, service, trackingNum);
+			pkgList.add(p);		
+		}
+
+		sc.close();
+
+		Connection conn = FedexDB.getConn();
+		FedexDB.clearDB();
+        for(Package p : pkgList)
+        {
+		    FedexDB.addPckgDet(conn, p);	
+        }
+		
+		PkgThread t[];
+        t = new PkgThread[pkgList.size()];
+
+		for(int i = 0; i < pkgList.size(); i++) 
+        {
+		  t[i] = new PkgThread(pkgList.get(i));
+          System.out.println("\nThread " + i + ", " + "Tracking Number = " + t[i].p.trackingNo);
+		  t[i].startThread();
+        }
     }
 }
